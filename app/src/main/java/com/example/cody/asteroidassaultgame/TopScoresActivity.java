983f6d774ff4c20f19253bcd7876f8ca1a4ef566
Cody Roof <codeman2727@gmail.com>
2017-12-10 21:41:58 -0700

    Made horrible code slightly less horrible

--- app/src/main/java/com/example/cody/asteroidassaultgame/TopScoresActivity.java
+++ app/src/main/java/com/example/cody/asteroidassaultgame/TopScoresActivity.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 public class TopScoresActivity extends Activity {
 
     DatabaseHelper mDatabaseHelper;
-    //private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +26,11 @@ public class TopScoresActivity extends Activity {
         Cursor data = mDatabaseHelper.getData();
         ArrayList<String> nameData = new ArrayList<>();
         ArrayList<String> scoreData = new ArrayList<>();
+
+        // WARNING -- HORRIBLE CODE BELOW -- CONTINUE AT YOUR OWN PERIL
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
         TextView nameText1 = findViewById(R.id.nameText);
         TextView scoreText1 = findViewById(R.id.scoreText);
@@ -59,43 +63,74 @@ public class TopScoresActivity extends Activity {
         TextView nameText15 = findViewById(R.id.nameText14);
         TextView scoreText15 = findViewById(R.id.scoreText14);
 
-
-
         while(data.moveToNext()){
             nameData.add(data.getString(1));
             scoreData.add(data.getString(2));
         }
-        nameText1.setText(nameData.get(0));
-        scoreText1.setText(scoreData.get(0));
-        nameText2.setText(nameData.get(1));
-        scoreText2.setText(scoreData.get(1));
-        nameText3.setText(nameData.get(2));
-        scoreText3.setText(scoreData.get(2));
-        nameText4.setText(nameData.get(3));
-        scoreText4.setText(scoreData.get(3));
-        nameText5.setText(nameData.get(4));
-        scoreText5.setText(scoreData.get(4));
-        nameText6.setText(nameData.get(5));
-        scoreText6.setText(scoreData.get(5));
-        nameText7.setText(nameData.get(6));
-        scoreText7.setText(scoreData.get(6));
-        nameText8.setText(nameData.get(7));
-        scoreText8.setText(scoreData.get(7));
-        nameText9.setText(nameData.get(8));
-        scoreText9.setText(scoreData.get(8));
-        nameText10.setText(nameData.get(9));
-        scoreText10.setText(scoreData.get(9));
-        nameText11.setText(nameData.get(10));
-        scoreText11.setText(scoreData.get(10));
-        nameText12.setText(nameData.get(11));
-        scoreText12.setText(scoreData.get(11));
-        nameText13.setText(nameData.get(12));
-        scoreText13.setText(scoreData.get(12));
-        nameText14.setText(nameData.get(13));
-        scoreText14.setText(scoreData.get(13));
-        nameText15.setText(nameData.get(14));
-        scoreText15.setText(scoreData.get(14));
-        //ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.list_content, nameData);
-       // mListView.setAdapter(adapter);
+        switch (nameData.size()) {
+            case 0:
+                nameText1.setText("No Scores Yet, time to play!");
+                break;
+            case 1:
+                nameText1.setText(nameData.get(0));
+                scoreText1.setText(scoreData.get(0));
+                break;
+            case 2:
+                nameText2.setText(nameData.get(1));
+                scoreText2.setText(scoreData.get(1));
+                break;
+            case 3:
+                nameText3.setText(nameData.get(2));
+                scoreText3.setText(scoreData.get(2));
+                break;
+            case 4:
+                nameText4.setText(nameData.get(3));
+                scoreText4.setText(scoreData.get(3));
+                break;
+            case 5:
+                nameText5.setText(nameData.get(4));
+                scoreText5.setText(scoreData.get(4));
+                break;
+            case 6:
+                nameText6.setText(nameData.get(5));
+                scoreText6.setText(scoreData.get(5));
+                break;
+            case 7:
+                nameText7.setText(nameData.get(6));
+                scoreText7.setText(scoreData.get(6));
+                break;
+            case 8:
+                nameText8.setText(nameData.get(7));
+                scoreText8.setText(scoreData.get(7));
+                break;
+            case 9:
+                nameText9.setText(nameData.get(8));
+                scoreText9.setText(scoreData.get(8));
+                break;
+            case 10:
+                nameText10.setText(nameData.get(9));
+                scoreText10.setText(scoreData.get(9));
+                break;
+            case 11:
+                nameText11.setText(nameData.get(10));
+                scoreText11.setText(scoreData.get(10));
+                break;
+            case 12:
+                nameText12.setText(nameData.get(11));
+                scoreText12.setText(scoreData.get(11));
+                break;
+            case 13:
+                nameText13.setText(nameData.get(12));
+                scoreText13.setText(scoreData.get(12));
+                break;
+            case 14:
+                nameText14.setText(nameData.get(13));
+                scoreText14.setText(scoreData.get(13));
+                break;
+            case 15:
+                nameText15.setText(nameData.get(14));
+                scoreText15.setText(scoreData.get(14));
+                break;
+        }
     }
 }
